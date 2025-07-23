29d1dbca29351dedce856604a1e0f0915ff68479
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-07-23 22:37:08 +0500

    operations in js

--- file.js
+++ file.js
@@ -1,9 +1,29 @@
-let someNumber = 33
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
-//  OPERATIONS 
+// let someNumber = 33
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+// //  OPERATIONS 
 
-let value = 3
-let negValue = -value;
-console.log();
+// let value = 3
+// let negValue = -value;
+// console.log();
+
+
+// let str1 = "hello ";
+// let str2 = " sabir";
+
+// let str3 = str1+ str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+let num1, num2, num3;
+
+num1=num2=num3=2+2;
+ 
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
