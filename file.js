1b6760f9b8cdb21c6c0dcae0cad07d77e7f081f5
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-07-24 21:06:41 +0500

    Update index.html and style.css

--- file.js
+++ file.js
@@ -20,10 +20,20 @@
 // console.log("1"+2+2);
 // console.log(1+2+"2");
 
-let num1, num2, num3;
 
-num1=num2=num3=2+2;
+
+// let num1, num2, num3;
+
+// num1=num2=num3=2+2;
  
-let gameCounter = 100
-++gameCounter;
-console.log(gameCounter);
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+
+console.log("2">1);
+console.log("02">1);
+
+
+console.log(null>0);
+console.log(null>0);
