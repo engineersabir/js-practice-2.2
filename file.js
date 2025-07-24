15408b7c42673cd16f925607f66bcfaa16270645
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-07-24 23:32:43 +0500

    Update index.html and style.css

--- file.js
+++ file.js
@@ -1,58 +1,59 @@
-// let someNumber = 33
-// let stringNumber = String(someNumber);
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
-// //  OPERATIONS 
+// // let someNumber = 33
+// // let stringNumber = String(someNumber);
+// // console.log(stringNumber);
+// // console.log(typeof stringNumber);
+// // //  OPERATIONS 
 
-// let value = 3
-// let negValue = -value;
-// console.log();
-
-
-// let str1 = "hello ";
-// let str2 = " sabir";
-
-// let str3 = str1+ str2;
-// console.log(str3);
-
-// console.log("1"+2);
-// console.log(1+"2");
-// console.log("1"+2+2);
-// console.log(1+2+"2");
+// // let value = 3
+// // let negValue = -value;
+// // console.log();
 
 
+// // let str1 = "hello ";
+// // let str2 = " sabir";
 
-// let num1, num2, num3;
+// // let str3 = str1+ str2;
+// // console.log(str3);
 
-// num1=num2=num3=2+2;
+// // console.log("1"+2);
+// // console.log(1+"2");
+// // console.log("1"+2+2);
+// // console.log(1+2+"2");
+
+
+
+// // let num1, num2, num3;
+
+// // num1=num2=num3=2+2;
  
-// let gameCounter = 100
-// ++gameCounter;
-// console.log(gameCounter);
+// // let gameCounter = 100
+// // ++gameCounter;
+// // console.log(gameCounter);
 
 
-// console.log("2">1);
-// console.log("02">1);
+// // console.log("2">1);
+// // console.log("02">1);
+
+
+// // console.log(null>0);
+// // console.log(null>0);
+// // console.log(null>=0);
 
 
 // console.log(null>0);
-// console.log(null>0);
-// console.log(null>=0);
+
+// // 2
+// console.log(null==0);
+// // 3
+// console.log(null=0);
+// // 4
+// console.log(undefined==0);
+// // 5
+// console.log(undefined>0);
+// // 6
+// console.log(undefined<0);
 
 
-console.log(null>0);
 
-// 2
-console.log(null==0);
-// 3
-console.log(null=0);
-// 4
-console.log(undefined==0);
-// 5
+
 console.log(undefined>0);
-// 6
-console.log(undefined<0);
-
-
-
-
