4f255e00dfb98bc090723d715ffdcae1961615fc
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-07-25 15:45:37 +0500

    Update index.html and style.css

--- file.js
+++ file.js
@@ -1,87 +1,95 @@
-// // let someNumber = 33
-// // let stringNumber = String(someNumber);
-// // console.log(stringNumber);
-// // console.log(typeof stringNumber);
-// // //  OPERATIONS 
+// // // let someNumber = 33
+// // // let stringNumber = String(someNumber);
+// // // console.log(stringNumber);
+// // // console.log(typeof stringNumber);
+// // // //  OPERATIONS 
 
-// // let value = 3
-// // let negValue = -value;
-// // console.log();
-
-
-// // let str1 = "hello ";
-// // let str2 = " sabir";
-
-// // let str3 = str1+ str2;
-// // console.log(str3);
-
-// // console.log("1"+2);
-// // console.log(1+"2");
-// // console.log("1"+2+2);
-// // console.log(1+2+"2");
+// // // let value = 3
+// // // let negValue = -value;
+// // // console.log();
 
 
+// // // let str1 = "hello ";
+// // // let str2 = " sabir";
 
-// // let num1, num2, num3;
+// // // let str3 = str1+ str2;
+// // // console.log(str3);
 
-// // num1=num2=num3=2+2;
+// // // console.log("1"+2);
+// // // console.log(1+"2");
+// // // console.log("1"+2+2);
+// // // console.log(1+2+"2");
+
+
+
+// // // let num1, num2, num3;
+
+// // // num1=num2=num3=2+2;
  
-// // let gameCounter = 100
-// // ++gameCounter;
-// // console.log(gameCounter);
+// // // let gameCounter = 100
+// // // ++gameCounter;
+// // // console.log(gameCounter);
 
 
-// // console.log("2">1);
-// // console.log("02">1);
+// // // console.log("2">1);
+// // // console.log("02">1);
+
+
+// // // console.log(null>0);
+// // // console.log(null>0);
+// // // console.log(null>=0);
 
 
 // // console.log(null>0);
-// // console.log(null>0);
-// // console.log(null>=0);
 
-
-// console.log(null>0);
-
-// // 2
-// console.log(null==0);
-// // 3
-// console.log(null=0);
-// // 4
-// console.log(undefined==0);
-// // 5
-// console.log(undefined>0);
-// // 6
-// console.log(undefined<0);
+// // // 2
+// // console.log(null==0);
+// // // 3
+// // console.log(null=0);
+// // // 4
+// // console.log(undefined==0);
+// // // 5
+// // console.log(undefined>0);
+// // // 6
+// // console.log(undefined<0);
 
 
 
 
-// console.log(undefined>0);
+// // console.log(undefined>0);
 
 
-// data types
+// // data types
 
-const score =100;
-const scoreValue =100.3;
-const isLoggedIn = false;
-const outsideTemp = null;
-let userEmail ;
-const id = symbol('123')
+// const score =100;
+// const scoreValue =100.3;
+// const isLoggedIn = false;
+// const outsideTemp = null;
+// let userEmail ;
+// const id = Symbol('123')
 
-const anothrId = symbol('123')
-console.log(id===anothrId);
+// const anothrId = Symbol('123')
+// console.log(id===anothrId);
 
-const myName= "sabir";
+// const myName= "sabir";
 
 
-let groceries=[apple,grapes, dates,peach,onion,]
+// let groceries=["apple","grapes", "dates","peach","onion","123","kevin"]
 
-let  Ottp= {
-name :"sabir",
-age:23,
-gender:m,
-isWeak:true,
-}
+// let  Ottp= {
+// name :"sabir",
+// age:23,
+// gender:"m",
+// isWeak:true,
+// }
     
-console.log(typeof groceries);
-console.log(typeof Ottp);
+// console.log(typeof groceries);
+// console.log(typeof Ottp);
+// console.log(typeof myName);
+ let arrey=["pakistan", "iran","indonesia", "japan"];
+  const personHe={
+    name:"tbh",
+    age:23,
+    race:"black",
+    nationality:"persian",
+  }
