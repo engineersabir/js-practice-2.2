0151ac62ecb4ffc0e5ea606d3a97742cd63f0656
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-08-02 14:16:34 +0500

    Update index.html and style.css

--- file.js
+++ file.js
@@ -166,6 +166,12 @@
   // let myCreatedDate = new Date ("2023-01-14")
   let myCreatedDate = new Date ("01-14-2023")
   console.log(myCreatedDate.toDateString());
+  let myTimeStamps=Date.now()
+  console.log(myTimeStamps);
+  console.log(myCreatedDate.getTime());
+  console.log(Date.now());
+  
+  
   
   
   
