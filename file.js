5226882a479f9f7fd3b58214bd5e1ac312875f65
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-08-03 17:48:39 +0500

    Update index.html and style.css

--- file.js
+++ file.js
@@ -194,16 +194,28 @@ const all_new_heroes=[...dc_heroes,...marvel_heroes]
 const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
 const real_another_array=another_array.flat(Infinity)
 // console.log(real_another_array);
-console.log(Array.isArray('hitesh'));
-console.log(Array.from('hitesh'));
+// console.log(Array.isArray('hitesh'));
+// console.log(Array.from('hitesh'));
 let score1=100
 let score2=200
 let score3=300
-console.log(Array.of(score1,score2,score3));
+// console.log(Array.of(score1,score2,score3));
+
+const jsUser={
+  name:"hitesh",
+  age:18,
+  location:"jaipur"
+  ,
+  email:"abc@gmail.com",
+  isLoggedIn:false,
+  lastloggedInDays:["monday","saturday"]
+}
+
+console.log(jsUser["age"]);
+console.log(jsUser.email);
 
 
 
-
   
   
   
@@ -211,7 +223,7 @@ console.log(Array.of(score1,score2,score3));
   
   
   
-  
+   
   
   
   
