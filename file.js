acdd1d08a299eb3672739fabbf25031f377637da
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-08-05 14:51:29 +0500

    Update index.html and style.css

--- file.js
+++ file.js
@@ -303,4 +303,26 @@
     two()
   }
   // one()
-  
+  if(true){
+    const username='hitesh'
+    if (username==='hitesh') {
+      const website = " youtube"
+      console.log(username +website);
+      
+      
+    }
+    // console.log(website);
+    
+  }
+  // console.log(username);
+   console.log(addone(5))
+  function addone(num){
+    return num + 1
+
+  }
+ 
+
+  const addtwo=function(num){
+    return num+2
+  }
+  console.log(addtwo(5))
