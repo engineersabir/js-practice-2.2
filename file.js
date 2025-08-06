b8d8b58c369e64fc28cfccce5b14f626ae40fdc4
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-08-06 14:35:17 +0500

    Update index.html and style.css

--- file.js
+++ file.js
@@ -368,15 +368,26 @@
 
    // MMEDIATELY INVOKED FUNCTION EXPRESSION (IIFE)
    
-(function chai(){
-  console.log('DB CONNECTED');
+// (function chai(){
+//   console.log('DB CONNECTED');
   
-})
-();
+// })
+// ();
    
-(
-    (name)=> {
-    console.log('DB CONNECTED TWO ${name}');
-    }
+// (
+//     (name)=> {
+//     console.log('DB CONNECTED TWO ${name}');
+//     }
   
-)('hitesh')
+// )('hitesh')
+
+// JAVASCRIPT  EXECUTION CONTEXT
+
+let val1=10
+let val2=5
+function addnum(num1,num2){
+  let total=num1+num2
+  return total
+}
+let result1=addnum(val1,val2)
+let result2=addnum(10,2)
