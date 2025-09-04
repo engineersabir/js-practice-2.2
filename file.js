82bd02ffeccff4929798fb8ce81ffe3cb7e64ed8
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-09-04 19:15:38 +0500

    Update index.html and style.css

--- file.js
+++ file.js
@@ -797,20 +797,31 @@ console.log(error)
   }
 }
 
-consumePromiseFive()
-async function getAllUsers(){
-try {
+// consumePromiseFive()
+// async function getAllUsers(){
+// try {
   
-  const response= await fetch(
-    'https://jsonplaceholder.typicode.com/users'
-  )
-  // console.log(response)
-  const data= await response.json()
-  console.log(data)
+//   const response= await fetch(
+//     'https://jsonplaceholder.typicode.com/users'
+//   )
+//   // console.log(response)
+//   const data= await response.json()
+//   console.log(data)
 
-} catch (error) {
-  console.log('E:',error)
-}
+// } catch (error) {
+//   console.log('E:',error)
+// }
 
-}
-getAllUsers()
+// }
+// getAllUsers()
+
+// fetch('https://jsonplaceholder.typicode.com/users'
+// )
+// .then((response)=>{
+//   return response.json()
+// })
+// .then((data)=>{
+//   console.log(data);
+// }).catch((error)=>{
+//   console.log(error)
+// })
