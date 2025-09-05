e2cd3d9c639f941144c1b1ba121fe63b4500fee0
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-09-05 18:32:39 +0500

    Update index.html and style.css

--- file.js
+++ file.js
@@ -732,54 +732,54 @@
 
 // })
 
-new Promise(function(resolve,reject){
-  setTimeout(function() {
-    console.log("async task2 is completed")
-    resolve()
-  }, 1000);
-}).then(function(){
-  console.log("async 2 resolved")
-})
-const promiseThree=new Promise(function(resolve,reject){
-  setTimeout(function(){
-    console.log('abcdef');
-    resolve({userName:"sabir",email:"chai or code@example.com"})
-  },1000)
-})
-promiseThree.then(function(user){
-  console.log(user);
-})
-const promiseFour=new Promise(function(resolve,reject){
-  setTimeout(function(){
-    let error=false
-    if(!error){
-      resolve({userName:'sabir',email:'abcdef@example.com'})
-    }else{
-      reject('ERROR:something went wrong')
-    }
-  },1000)
-})
-promiseFour.then((user)=>{
-  console.log(user);
-  return user.userName
-}).then((userName)=>{
-  console.log(userName);
-}).catch(function(error){
-console.log(error);
-}).finally(()=>{
-  console.log('the promise is either resolved or rejected')
-})
+// new Promise(function(resolve,reject){
+//   setTimeout(function() {
+//     console.log("async task2 is completed")
+//     resolve()
+//   }, 1000);
+// }).then(function(){
+//   console.log("async 2 resolved")
+// })
+// const promiseThree=new Promise(function(resolve,reject){
+//   setTimeout(function(){
+//     console.log('abcdef');
+//     resolve({userName:"sabir",email:"chai or code@example.com"})
+//   },1000)
+// })
+// promiseThree.then(function(user){
+//   console.log(user);
+// })
+// const promiseFour=new Promise(function(resolve,reject){
+//   setTimeout(function(){
+//     let error=false
+//     if(!error){
+//       resolve({userName:'sabir',email:'abcdef@example.com'})
+//     }else{
+//       reject('ERROR:something went wrong')
+//     }
+//   },1000)
+// })
+// promiseFour.then((user)=>{
+//   console.log(user);
+//   return user.userName
+// }).then((userName)=>{
+//   console.log(userName);
+// }).catch(function(error){
+// console.log(error);
+// }).finally(()=>{
+//   console.log('the promise is either resolved or rejected')
+// })
 
-const promiseFive=new Promise(function(resolve,reject){
-  setTimeout(function(){
-let error=false
-if (!error){
-  resolve({userName:'javaScript',password:'1234'})
-}else{
-  reject('ERROR: JS went wrong')
-}
-  },1000)
-});
+// const promiseFive=new Promise(function(resolve,reject){
+//   setTimeout(function(){
+// let error=false
+// if (!error){
+//   resolve({userName:'javaScript',password:'1234'})
+// }else{
+//   reject('ERROR: JS went wrong')
+// }
+//   },1000)
+// });
 
 
 // async function consumePromiseFive(){
@@ -788,14 +788,14 @@ if (!error){
 // }
 // consumePromiseFive()
 
-async function consumePromiseFive(){
-  try{
-    const response=await promiseFive
-    console.log(response);
-  }catch(error){
-console.log(error)
-  }
-}
+// async function consumePromiseFive(){
+//   try{
+//     const response=await promiseFive
+//     console.log(response);
+//   }catch(error){
+// console.log(error)
+//   }
+// }
 
 // consumePromiseFive()
 // async function getAllUsers(){
@@ -825,3 +825,17 @@ console.log(error)
 // }).catch((error)=>{
 //   console.log(error)
 // })
+
+const user={
+  userName:'hitesh',
+  logInCount:8,
+   signIn:true,
+   getUserDetails: function(){
+    // console.log(`userName:${this.userName}`)
+  console.log(this) 
+  
+  }
+}
+console.log(user.logInCount);
+console.log(user.getUserDetails());
+console.log(this)
---
+++ notes.md
@@ -0,0 +1,23 @@
+# java script and classes
+
+## OOP 
+## object 
+
+- collection of properties and methods
+- toLowerCase
+
+## why use oop
+
+## parts of oop
+ Object literal
+  - constructor function 
+  - prototypes
+  - classes Instances (new, this)
+
+
+  ## Pillars 
+  
+  Abstsraction
+  Encapsulation
+  Inheritance
+  Polymorphism
