66041579a3f559482dc0dca648ade1eade3d8879
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-09-09 14:52:16 +0500

    Update index.html and style.css

--- file.js
+++ file.js
@@ -1045,12 +1045,50 @@
 //   }
   
 // }
- class User {
-  constructor (email,password){
-    this.email=email;
-    this.password=password
 
+//  class User {
+//   constructor (email,password){
+//     this.email=email;
+//     this.password=password
+
+//   }
+//  }
+//  const hitesh=new User('hitesh@h.ai','321')
+//  console.log(hitesh.email)
+function user (email,password){
+  this._email=email;
+  this._password=password
+
+  Object.defineProperty(this,'email',{
+    get:function(){
+      return this._email.toUpperCase()
+    },
+    set:function(value){
+      this.email=value
+    }
+  })
+  Object.defineProperty(this,'password',{
+    get:function(){
+      return this._password.toUpperCase()
+    },
+    set:function(value){
+      this.password=value
+    }
+  })
+}
+const chai=new user('chai@ch.com','abcdefghijklmnopqrstuvwxyz')
+// console.log(chai.email)
+// console.log(chai.password)
+
+const User1={
+  _email:'abc@cmail.com',
+  _password:'xyz',
+  get email(){
+    return this._email.toUpperCase()
+  },
+  set email(value){
+this._email=value
   }
- }
- const hitesh=new User('hitesh@h.ai','321')
- console.log(hitesh.email)
+}
+const tea=Object.create(User1)
+console.log(tea.email)
