fd1d8a6a8ba74a8e188615e5d81ab4a4fa437e42
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-09-16 22:38:05 +0500

    Update index.html and style.css

--- variable.js
+++ variable.js
@@ -22,3 +22,82 @@ console.log(bd);
     console.log(b);
 }
 console.log(b);
+
+let firstName="elon";
+let lastName='Musk';
+console.log(firstName+lastName);
+console.log( ` your name is  ${firstName+lastName}`);
+ let num= 96.0;
+ console.log(num);
+
+// Boolean Data types 
+
+let learning=true;
+let completed=false;
+console.log(learning);
+console.log( typeof completed);
+console.log(completed);
+
+let x=20<10;
+console.log(x);
+console.log(typeof x);
+
+// it ahs two values : true and false.
+
+
+// Undefined Data Types
+
+let age;
+console.log(age);
+console.log(typeof age);
+
+
+//
+
+// Null Data Type
+let number=null;
+console.log(number);
+console.log(null===undefined);
+//
+
+// Referencde DataType
+
+let person={
+    firstName:'Elon',
+    lastName:'Musk',
+    age:35
+};
+console.log(person);
+console.log(typeof person)
+
+//
+
+// Array Data Type
+
+let number2=[1,2,3,4,5]
+console.log(number2);
+console.log(typeof number2);
+//
+
+// Function Data Type
+function msg(){
+    console.log("Hello , Sabir bhai")
+}
+console.log(typeof msg);
+//
+let x2;
+console.log(x2);
+console.log(typeof x2);
+
+x2="great Work";
+console.log(x2);
+console.log(typeof x2);
+
+x2=100;
+console.log(x2);
+console.log(typeof x2);
+
+x2=true;
+
+console.log(x2);
+console.log(typeof x2);
