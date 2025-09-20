252784ed6335a85ead1b910e81c25353873bb0fc
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-09-20 17:11:58 +0500

    Update index.html and style.css

--- arraysloops.js
+++ arraysloops.js
@@ -1,4 +1,6 @@
-// let marvelHeroes=["thor","spiderman","ironman", "antman","Dr. strange"]
+console.log("JS file is connected!");
+
+//  let marvelHeroes=["thor","spiderman","ironman", "antman","Dr. strange"]
 // console.log(marvelHeroes);
 
 // // console.log(marvelHeroes.slice(1,3));
@@ -6,22 +8,32 @@
 // console.log(marvelHeroes.slice(1));
 // console.log(marvelHeroes.slice(0));
 
-let arr=[1,2,3,4,5,6,7
-]
-// arr.splice(2,3,123,345,567)
-// add an Element
-// arr.splice(2,0,101);
-// arr.splice(3,1);
-// arr.splice(3,1,789);
-arr.splice(4);
-// console.log(arr);
-let companies=["Boomberg","microsoft","Uber","Google","IBM","Netflix"]
-// remove the first company
-// companies.shift();
-console.log(companies);
-
-// Remove  Uber and Add Ola in its place
-// companies.splice(2,1,"Ola");
+// let arr=[1,2,3,4,5,6,7
+// ]
+// // arr.splice(2,3,123,345,567)
+// // add an Element
+// // arr.splice(2,0,101);
+// // arr.splice(3,1);
+// // arr.splice(3,1,789);
+// arr.splice(4);
+// // console.log(arr);
+// let companies=["Boomberg","microsoft","Uber","Google","IBM","Netflix"]
+// // remove the first company
+// // companies.shift();
 // console.log(companies);
-companies.push("Amazon");
-console.log(companies);
+
+// // Remove  Uber and Add Ola in its place
+// // companies.splice(2,1,"Ola");
+// // console.log(companies);
+// companies.push("Amazon");
+// console.log(companies);
+
+// for (let i=1;i<=10;i++){
+//     console.log(" YA ALI MADAD...!")
+// }
+let sum =0;
+for (let i=1;i<=8;i++){
+    sum=sum+i;
+}
+console.log("sum=",sum);
+console.log("Loop Has Been Ended.")
