ae4de699033254009f8e0ffc7a34d030c96ed4d3
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-09-22 19:11:33 +0500

    Update index.html and style.css

--- function.js
+++ function.js
@@ -1,50 +1,52 @@
-// // // sum finction
-// // function sum(a,b){
-// //     return a+b;
+// // // // sum finction
+// // // function sum(a,b){
+// // //     return a+b;
+// // // }
+// // // // multiplication function
+// // // function mul(a,b){
+// // //     return a*b;
+// // // }
+// // // // arrow function
+// // // const arrowMul=(a,b)=>{
+// // //     return a*c;
+// // // };
+// // // arrowMul=8;
+
+// // // create a function using the "function" keyword that takes a string as an argument& returns the number of vowels in the strings.
+// // let countVowels= function(){
+// //     const string ="hello";
+// //     console.log(string);
+// // const vowelsCount=()=>{
+// //     if(string.includes("a,e,i,o,u"))
+// //         console.log(countVowels);
 // // }
-// // // multiplication function
-// // function mul(a,b){
-// //     return a*b;
 // // }
-// // // arrow function
-// // const arrowMul=(a,b)=>{
-// //     return a*c;
-// // };
-// // arrowMul=8;
+// // let arr=["abcdefghijklmnopqrstuvwxyz","not","one"]
+// // arr.forEach((val)=>{
+// //     console.log(val.toUpperCase());
+// // });
 
-// // create a function using the "function" keyword that takes a string as an argument& returns the number of vowels in the strings.
-// let countVowels= function(){
-//     const string ="hello";
-//     console.log(string);
-// const vowelsCount=()=>{
-//     if(string.includes("a,e,i,o,u"))
-//         console.log(countVowels);
-// }
-// }
-// let arr=["abcdefghijklmnopqrstuvwxyz","not","one"]
-// arr.forEach((val)=>{
-//     console.log(val.toUpperCase());
-// });
+// // // for a given array of numbers, print the square of each value using the forEach loop.
+// // let num=[1,2,3,4,5]
+// // num.forEach((val)=>{
+// // console.log(val*val)
+// // }) ;
 
-// // for a given array of numbers, print the square of each value using the forEach loop.
-// let num=[1,2,3,4,5]
-// num.forEach((val)=>{
-// console.log(val*val)
-// }) ;
+//  let nums =[23,34,56];
 
- let nums =[23,34,56];
+//  let newArr=nums.map((val)=>{
+//     return val*2;
+//  })
+//  console.log(newArr);
 
- let newArr=nums.map((val)=>{
-    return val*2;
- })
- console.log(newArr);
+//  let calSquare=(num)=>{
+//     console.log(num*num);
+//  }
 
- let calSquare=(num)=>{
-    console.log(num*num);
- }
-
- let arr=[1,2,3,4,5,6,7]
- let evenArr=arr.filter((val)=>{
-    return val%2===0;
- })
- console.log(evenArr);
+//  let arr=[1,2,3,4,5,6,7]
+//  let evenArr=arr.filter((val)=>{
+//     return val%2===0;
+//  })
+//  console.log(evenArr);
+//console.clear();
+// alert("hello");
