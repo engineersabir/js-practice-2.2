587da334e1d1d7cdb606927c969a010fe1840986
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-09-25 11:29:17 +0500

    Update index.html and style.css

--- domevent.js
+++ domevent.js
@@ -1,17 +1,17 @@
-// function upperCase(){
-//     const x=document.getElementById("fname");
-//     x.value=x.value.toUpperCase();
-// }
+// // function upperCase(){
+// //     const x=document.getElementById("fname");
+// //     x.value=x.value.toUpperCase();
+// // }
 
-// let msg=document.getElementById("message");
+// // let msg=document.getElementById("message");
+// // console.log(msg);
+// // let btn=document.getElementsByName("language");
+// // console.log(btn);
+// // let headings=document.getElementsByTagName("h1");
+// // console.log(headings);
+// // let classes=document.getElementsByClassName("message");
+// // console.log(classes);
+
+// let cont=document.getElementById("container");
+// let msg=document.querySelectorAll("div , h1");
 // console.log(msg);
-// let btn=document.getElementsByName("language");
-// console.log(btn);
-// let headings=document.getElementsByTagName("h1");
-// console.log(headings);
-// let classes=document.getElementsByClassName("message");
-// console.log(classes);
-
-let cont=document.getElementById("container");
-let msg=document.querySelectorAll("div , h1");
-console.log(msg);
