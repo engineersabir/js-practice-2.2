575ed251c113b8caf056962a86217f10c2d287c9
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-09-29 10:16:38 +0500

    Update index.html and style.css

--- obj&method/file.js
+++ obj&method/file.js
@@ -1,71 +1,82 @@
-// // // const student={
-// // //     fullName:"sabir",
-// // //     marks:90,
-// // //     printMarks:function(){
-// // //         console.log("marks =",this.marks);
-// // //     }
+// // // // const student={
+// // // //     fullName:"sabir",
+// // // //     marks:90,
+// // // //     printMarks:function(){
+// // // //         console.log("marks =",this.marks);
+// // // //     }
+// // // // }
+// // // // console.log(student);
+// // // const employee={
+// // //     calcTax(){
+// // //         console.log("tax rate is 89%")
+// // //     },
 // // // }
-// // // console.log(student);
-// // const employee={
-// //     calcTax(){
-// //         console.log("tax rate is 89%")
-// //     },
-// // }
 
-// // const karanBhai={
-// //     salary:500,
-// // }
+// // // const karanBhai={
+// // //     salary:500,
+// // // }
 
 
-// // const karanBhai2={
-// //     salary:500,
-// // }
-// // const karanBhai3={
-// //     salary:500,
-// // }
-// // const karanBhai4={
-// //     salary:500,
-// // };
-// // karanBhai.__proto__=employee;
-// // karanBhai3.__proto__=employee;
-// // karanBhai2.__proto__=employee;
-// // karanBhai4.__proto__=employee;
+// // // const karanBhai2={
+// // //     salary:500,
+// // // }
+// // // const karanBhai3={
+// // //     salary:500,
+// // // }
+// // // const karanBhai4={
+// // //     salary:500,
+// // // };
+// // // karanBhai.__proto__=employee;
+// // // karanBhai3.__proto__=employee;
+// // // karanBhai2.__proto__=employee;
+// // // karanBhai4.__proto__=employee;
 
-// class toyotaCar{
+// // class toyotaCar{
 
-//     start(){
-//         console.log("start")
-//     }
+// //     start(){
+// //         console.log("start")
+// //     }
 
     
-//     stop(){
-//         console.log("stop")
-//     }
+// //     stop(){
+// //         console.log("stop")
+// //     }
 
-//     setBrand(){
-//         this.brandName=this.brand;
+// //     setBrand(){
+// //         this.brandName=this.brand;
+// //     }
+// // }
+// // let fortuner=new toyotaCar();
+// // fortuner.setBrand("fortuner");
+// // let lexus=new toyotaCar();
+// // lexus.setBrand(lexus);
+// class person{
+//     eat(){
+//         console.log("eat")
+//     }
+//     sleep(){
+//         console.log("sleep")
 //     }
 // }
-// let fortuner=new toyotaCar();
-// fortuner.setBrand("fortuner");
-// let lexus=new toyotaCar();
-// lexus.setBrand(lexus);
-class person{
-    eat(){
-        console.log("eat")
-    }
-    sleep(){
-        console.log("sleep")
-    }
-}
-class Engineer{
-    work(){
-        console.log("solve problems , build something")
-    }
-}
-class Doctor{
-    work(){
-        console.log("treat patients");
-    }
-}
-let Danish=new Engineer();
+// class Engineer{
+//     work(){
+//         console.log("solve problems , build something")
+//     }
+// }
+// class Doctor{
+//     work(){
+//         console.log("treat patients");
+//     }
+// }
+// let Danish=new Engineer();
+
+const food = ['apple','peach','cucumber','papaya','dates','apricoat']
+
+// const peach=food.map((item)=>'peach')
+// console.log(peach);
+// const duplicateFood=food.map((any)=>any+any)
+// ;
+// console.log(duplicateFood);
+
+const fruits=food.filter((item)=>item.type==='fruite');
+console.log(fruits)
