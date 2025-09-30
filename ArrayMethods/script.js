6e88e2794fa0d3cebaaa6dd47b800bbb1115e467
sabir-pseudo <sabirfareedmalik@gmail.com>
2025-09-30 21:44:54 +0500

    Update index.html and style.css

---
+++ ArrayMethods/script.js
@@ -0,0 +1,52 @@
+// const food=["🍎,","🥕","🍐","🍆","🍌","🍅","🥑"];
+// const bananaIndex=food.indexOf("🍌");
+// console.log(bananaIndex); 
+// food.some((item)=>item==='🍌'
+
+// )
+
+const months=["january", "february","March", "April"];
+
+// add element in start
+// months.push('May');
+// remove element from end
+
+// months.pop();
+// months.pop()
+// console.log(months);
+
+// SHIFT() METHOD
+
+// months.shift();
+// months.shift();
+
+// UNSHIFT METHOD
+
+// let abc=['january', 'february', 'december','september']
+
+// months.unshift(abc);
+
+
+// CONCAT METHOD
+
+// const months2=['june','july', 'august','september']
+// const yearHalf=months.concat(months2);
+
+
+//JOIN METHOD
+
+// const text=months.join(' and ');
+// console.log(text);
+
+//IS ARRAY METHOD
+
+// const text='king of india'
+// const text2=true;
+// const text3=56;
+
+// const resultArray=Array.isArray(months);
+// console.log(resultArray);
+
+
+// FOR EACH METHOD
+months.forEach
